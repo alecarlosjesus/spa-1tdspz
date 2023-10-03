2db9d8962131e56f4487b2fda803b65c6136b724
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2023-10-02 22:20:01 -0300

    Recriando o componente App adicionado imgs estáticas.

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+export default function App(){
+  document.title = "HOME";
+
+  return(
+    <div>
+        <h1>App</h1>
+        <div>
+          <figure>
+            <figcaption>
+              Produtos em Oferta...
+            </figcaption>
+            <img src="/img/grocery_640.jpg" alt="Produtos diversos." />
+          </figure>
+        </div>  
+    </div>
+  )
+}
